3feb47d44cc5d4ceb28e46864a0224840c04369f
abhaikumar10 <abhaikumar10@users.noreply.github.com>
2020-08-22 15:28:42 +0530

    Outline for new website

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@
       $('#preloader').delay(100).fadeOut('slow', function() {
         $(this).remove();
       });
-    }
+   }
   });
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
