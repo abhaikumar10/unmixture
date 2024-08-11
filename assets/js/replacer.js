5d461a51c6885f41ba31c975582bec7e526c8137
abhaikumar10 <abhaikumar10@gmail.com>
2024-08-11 13:04:08 +0530

    add

---
+++ assets/js/replacer.js
@@ -0,0 +1,19 @@
+<!--Head replace --> 
+
+        // Function to replace {{head}} with content from an external file
+        function replacePlaceholderWithHead(content) {
+            // Find the placeholder {{head}} in the document head
+            const headContent = document.head.innerHTML;
+            const newHeadContent = headContent.replace('{{head}}', content);
+
+            // Replace the head content with the new content
+            document.head.innerHTML = newHeadContent;
+        }
+
+        // Fetch the head content from another HTML file
+        fetch('\include\header.js') 
+            .then(response => response.text())
+            .then(data => {
+                replacePlaceholderWithHead(data);
+            })
+            .catch(error => console.error('Error fetching the head content:', error));
